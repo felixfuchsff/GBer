3fcc5b4252992aab4d9f5cf9414833bf989895f4
Felix <noreplay-felixff@gihub.com>
2025-10-19 04:58:09 +0200

    move code to src

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url)) + "/src"
 
 export default defineConfig({
+    root: "src",
     base: "./",
     build: {
+        outDir: "../dist",
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
